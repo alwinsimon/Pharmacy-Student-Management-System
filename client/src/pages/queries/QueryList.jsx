0b02c8dc340332326a52a5ca37c9717affe6b65e
Dr. Alwin Simon <003alwin@gmail.com>
2025-05-25 15:25:25 +0530

    Resolving hosting port error

--- client/src/pages/queries/QueryList.jsx
+++ client/src/pages/queries/QueryList.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import {
   Box,
   Button,
